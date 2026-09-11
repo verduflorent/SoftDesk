52db3d567a549948958e8f3f980f1c9ba79e6726
verduflorent <verdu.florent@gmail.com>
2026-09-11 16:30:53 +0200

    Extend API security regression tests

--- projects/tests.py
+++ projects/tests.py
@@ -14,8 +14,8 @@ class SoftDeskUserJourneyTests(APITestCase):
         self.member = User.objects.create_user(username="Member", password="testpass123", age=25)
         self.outsider = User.objects.create_user(username="Outsider", password="testpass123", age=25)
         self.project = Project.objects.create(name="Project A", description="Test", type=Project.Type.BACK_END, author=self.owner)
-        Contributor.objects.create(user=self.owner, project=self.project)
-        Contributor.objects.create(user=self.member, project=self.project)
+        Contributor.objects.create(user=self.owner, project=self.project, author=self.owner)
+        Contributor.objects.create(user=self.member, project=self.project, author=self.owner)
         self.issue = Issue.objects.create(title="Bug", description="Test", priority=Issue.Priority.HIGH, tag=Issue.Tag.BUG, project=self.project, author=self.member, assignee=self.owner)
         self.comment = Comment.objects.create(description="Comment", issue=self.issue, author=self.member)
 
@@ -27,8 +27,9 @@ class SoftDeskUserJourneyTests(APITestCase):
         response = self.client.post(reverse("project-list"), {"name": "New Project", "description": "Test", "type": Project.Type.FRONT_END}, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         project = Project.objects.get(pk=response.data["id"])
+        contribution = Contributor.objects.get(user=self.outsider, project=project)
         self.assertEqual(project.author, self.outsider)
-        self.assertTrue(Contributor.objects.filter(user=self.outsider, project=project).exists())
+        self.assertEqual(contribution.author, self.outsider)
 
     def test_project_access_depends_on_membership(self):
         self.login_as(self.member)
@@ -60,6 +61,18 @@ class SoftDeskUserJourneyTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("assignee", response.data)
 
+    def test_issue_author_cannot_move_issue_to_inaccessible_project(self):
+        private_project = Project.objects.create(name="Private", description="Test", type=Project.Type.BACK_END, author=self.outsider)
+        Contributor.objects.create(user=self.outsider, project=private_project, author=self.outsider)
+
+        self.login_as(self.member)
+        response = self.client.patch(
+            reverse("issue-detail", args=[self.issue.pk]),
+            {"project": private_project.pk, "assignee": None},
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
     def test_only_comment_author_can_delete_comment(self):
         url = reverse("comment-detail", args=[self.comment.uuid])
         self.login_as(self.owner)
@@ -68,6 +81,19 @@ class SoftDeskUserJourneyTests(APITestCase):
         self.login_as(self.member)
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_comment_author_cannot_move_comment_to_inaccessible_issue(self):
+        private_project = Project.objects.create(name="Private", description="Test", type=Project.Type.BACK_END, author=self.outsider)
+        Contributor.objects.create(user=self.outsider, project=private_project, author=self.outsider)
+        private_issue = Issue.objects.create(title="Private bug", description="Test", priority=Issue.Priority.LOW, tag=Issue.Tag.BUG, project=private_project, author=self.outsider)
+
+        self.login_as(self.member)
+        response = self.client.patch(
+            reverse("comment-detail", args=[self.comment.uuid]),
+            {"issue": private_issue.pk},
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_outsider_lists_are_empty(self):
         self.login_as(self.outsider)
