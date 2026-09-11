9185a6c515b6c779af853c9f4b3a7abe3313b624
verduflorent <verdu.florent@gmail.com>
2026-09-11 16:23:42 +0200

    Add API user journey tests

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,77 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .models import Comment, Contributor, Issue, Project
+
+User = get_user_model()
+
+
+class SoftDeskUserJourneyTests(APITestCase):
+    def setUp(self):
+        self.owner = User.objects.create_user(username="Owner", password="testpass123", age=25)
+        self.member = User.objects.create_user(username="Member", password="testpass123", age=25)
+        self.outsider = User.objects.create_user(username="Outsider", password="testpass123", age=25)
+        self.project = Project.objects.create(name="Project A", description="Test", type=Project.Type.BACK_END, author=self.owner)
+        Contributor.objects.create(user=self.owner, project=self.project)
+        Contributor.objects.create(user=self.member, project=self.project)
+        self.issue = Issue.objects.create(title="Bug", description="Test", priority=Issue.Priority.HIGH, tag=Issue.Tag.BUG, project=self.project, author=self.member, assignee=self.owner)
+        self.comment = Comment.objects.create(description="Comment", issue=self.issue, author=self.member)
+
+    def login_as(self, user):
+        self.client.force_authenticate(user=user)
+
+    def test_project_creation_adds_creator_as_contributor(self):
+        self.login_as(self.outsider)
+        response = self.client.post(reverse("project-list"), {"name": "New Project", "description": "Test", "type": Project.Type.FRONT_END}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        project = Project.objects.get(pk=response.data["id"])
+        self.assertEqual(project.author, self.outsider)
+        self.assertTrue(Contributor.objects.filter(user=self.outsider, project=project).exists())
+
+    def test_project_access_depends_on_membership(self):
+        self.login_as(self.member)
+        response = self.client.get(reverse("project-detail", args=[self.project.pk]))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.login_as(self.outsider)
+        response = self.client.get(reverse("project-detail", args=[self.project.pk]))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_only_project_author_can_modify_project(self):
+        self.login_as(self.member)
+        response = self.client.patch(reverse("project-detail", args=[self.project.pk]), {"name": "Blocked"}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.login_as(self.owner)
+        response = self.client.patch(reverse("project-detail", args=[self.project.pk]), {"name": "Allowed"}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_only_issue_author_can_modify_issue(self):
+        self.login_as(self.owner)
+        response = self.client.patch(reverse("issue-detail", args=[self.issue.pk]), {"title": "Blocked"}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.login_as(self.member)
+        response = self.client.patch(reverse("issue-detail", args=[self.issue.pk]), {"title": "Allowed"}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_non_contributor_cannot_be_assigned_to_issue(self):
+        self.login_as(self.member)
+        response = self.client.patch(reverse("issue-detail", args=[self.issue.pk]), {"assignee": self.outsider.pk}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("assignee", response.data)
+
+    def test_only_comment_author_can_delete_comment(self):
+        url = reverse("comment-detail", args=[self.comment.uuid])
+        self.login_as(self.owner)
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.login_as(self.member)
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_outsider_lists_are_empty(self):
+        self.login_as(self.outsider)
+        for route in ("project-list", "issue-list", "comment-list"):
+            response = self.client.get(reverse(route))
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(response.data["count"], 0)
