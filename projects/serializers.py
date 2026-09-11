a60f8ebcfa4848bb2111479fdecfe987cf886b04
verduflorent <verdu.florent@gmail.com>
2026-09-11 15:05:18 +0200

    Add issue serializer

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Contributor, Project
+from .models import Contributor, Issue, Project
 
 
 class ProjectSerializer(serializers.ModelSerializer):
@@ -34,3 +34,48 @@ class ContributorSerializer(serializers.ModelSerializer):
         read_only_fields = [
             "created_time",
         ]
+
+
+class IssueSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source="author.username")
+
+    class Meta:
+        model = Issue
+        fields = [
+            "id",
+            "title",
+            "description",
+            "priority",
+            "tag",
+            "status",
+            "project",
+            "author",
+            "assignee",
+            "created_time",
+        ]
+        read_only_fields = [
+            "author",
+            "created_time",
+        ]
+
+    def validate(self, data):
+        project = data.get("project")
+        assignee = data.get("assignee")
+
+        if assignee and project:
+            is_contributor = Contributor.objects.filter(
+                user=assignee,
+                project=project,
+            ).exists()
+
+            if not is_contributor:
+                raise serializers.ValidationError(
+                    {
+                        "assignee": (
+                            "L'utilisateur assigné doit être "
+                            "contributeur du projet."
+                        )
+                    }
+                )
+
+        return data
