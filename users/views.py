60a321aef624dbde44950e2c13dcf3d06396789c
verduflorent <verdu.florent@gmail.com>
2026-09-11 15:35:35 +0200

    Restrict user profile access

--- users/views.py
+++ users/views.py
@@ -1,11 +1,18 @@
-from rest_framework import viewsets
+from rest_framework import permissions, viewsets
 
 from .models import User
 from .serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
     serializer_class = UserSerializer
 
-# Create your views here.
+    def get_permissions(self):
+        if self.action == "create":
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return User.objects.filter(pk=self.request.user.pk)
+        return User.objects.none()
